3829bf103e38af617d2323287858f86490673490
Hesham Eldesoky <hishameldesoky111@gmail.com>
2025-07-03 14:43:07 +0300

    Hyberparameter training

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ import dill
 import numpy as np 
 import pandas as pd 
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.logger import logging
 from src.exception import CustomException
@@ -27,14 +28,18 @@ def save_object(file_path , obj):
         raise CustomException(e , sys)
     
 
-def evaluate_model(x_train,y_train,x_test ,y_test , models):
+def evaluate_model(x_train,y_train,x_test ,y_test , models , params):
 
     try :
         report = {}
 
         for i  in range(len(list(models))):
             model = list(models.values())[i]
-            
+            param = params[list(models.keys())[i]]
+
+            gs= GridSearchCV(model , param )
+            gs.fit(x_train , y_train)
+            model.set_params(**gs.best_params_)
             model.fit(x_train , y_train)
 
             y_train_pred = model.predict(x_train)
